7188fc94a1e13363a63f7bc8eacbdbe6c57fc69f
Randriamitsiry <tsiryjessgabriel@gmail.com>
2018-05-06 10:55:19 +0400

    contenu

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,10 +10,25 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/accueil", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
-        return $this->render("::accueil.html.twig");
+        return $this->render("page/accueil.html.twig");
+    }
+
+    /**
+     * @Route("/search/{query}", name="search")
+     */
+    public function searchAction($query)
+    {
+        return $this->render("page/search.html.twig");
+    }
+    /**
+     * @Route("/zone/rouge", name="zone_rouge")
+     */
+    public function zonerougeAction()
+    {
+        return $this->render("page/zonerouge.html.twig");
     }
 }
